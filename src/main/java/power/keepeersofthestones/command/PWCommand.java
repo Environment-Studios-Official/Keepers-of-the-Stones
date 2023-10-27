7c21e07bbd33760e51c424f75fbe5db7999819e6
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2023-10-27 19:53:16 +0300

    Replaced the argument of the pw rechoice command with reset

--- src/main/java/power/keepeersofthestones/command/PWCommand.java
+++ src/main/java/power/keepeersofthestones/command/PWCommand.java
@@ -30,7 +30,7 @@ import com.mojang.brigadier.arguments.DoubleArgumentType;
 public class PWCommand {
 	@SubscribeEvent
 	public static void registerCommand(RegisterCommandsEvent event) {
-		event.getDispatcher().register(Commands.literal("pw").requires(s -> s.hasPermission(4)).then(Commands.literal("stone").then(Commands.literal("rechoice").then(Commands.argument("name", EntityArgument.player()).executes(arguments -> {
+		event.getDispatcher().register(Commands.literal("pw").requires(s -> s.hasPermission(4)).then(Commands.literal("stone").then(Commands.literal("reset").then(Commands.argument("name", EntityArgument.player()).executes(arguments -> {
 			Level world = arguments.getSource().getUnsidedLevel();
 			double x = arguments.getSource().getPosition().x();
 			double y = arguments.getSource().getPosition().y();
